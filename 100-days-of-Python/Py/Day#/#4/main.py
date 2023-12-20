dd7414316bd1a3cdf18210de6b67170922c81c95
Sarath K J <kjsarath76@gmail.com>
2023-12-20 08:00:46 +0530

    Day 4 beginning

--- 100-days-of-Python/Py/Day#/#4/main.py
+++ 100-days-of-Python/Py/Day#/#4/main.py
@@ -1,1 +1,9 @@
 #randomisation and python list
+import random
+
+# #randint
+# random_integer=random.randint(0,10)
+# print(random_integer)
+# #random float
+# random_float = random.random()
+# print(random_float)
